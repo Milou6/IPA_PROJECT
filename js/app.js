ab6e35d3a5717e34f23afd77676ec8479f4068d0
Adoxography <gstill@uw.edu>
2019-01-29 18:29:22 -0600

    Move the symbol variables into symbols.js

--- js/app.js
+++ js/app.js
@@ -1,142 +1,74 @@
-//		mɱnɳɲŋɴpbtdʈɖcɟkgqɢʡʔɸβfvθðszʃʒʂʐɕʑçʝxɣχʁħʕʜʢhɦʋɹɻjɰⱱɾɽʙrʀɬɮlɭʎʟɺ
-//		iyɨʉɯuɪʏʊeøɘɵɤoəɛœɜɞʌɔæɐaɶɑɒ
-//		"i","y","ɨ","ʉ","ɯ","u","ɪ","ʏ","ʊ","e","ø","ɘ","ɵ","ɤ","o","ə","ɛ","œ","ɜ","ɞ","ʌ","ɔ","æ","ɐ","a","ɶ","ɑ","ɒ"
+var toAdd = [];
 
-		var allSymbols = ["m","ɱ","n","ɳ","ɲ","ŋ","ɴ","p","b","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ʔ","ɔ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ","ʋ","ɹ","ɻ","j","ɰ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɬ","ɮ","l","ɭ","ʎ","ʟ","ɺ","i","y","ɨ","ʉ","ɯ","u","ɪ","ʏ","ʊ","e","ø","ɘ","ɵ","ɤ","o","ə","ɛ","œ","ɜ","ɞ","ʌ","ɔ","æ","ɐ","a","ɶ","ɑ","ɒ"];
-		var activeFeatures = 0;
-		var displayedSymbols = allSymbols;
-		
-		var featuresObject = {
+function addFeature(featureId) {													//TO REMOVE...............................................
 
-		plussyllabic: [],
-		minussyllabic: ["m","ɱ","n","ɳ","ɲ","ŋ","ɴ","p","b","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ʔ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ","ʋ","ɹ","ɻ","j","ɰ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɬ","ɮ","l","ɭ","ʎ","ʟ","ɺ"],
-		plusconsonantal: ["m","ɱ","n","ɳ","ɲ","ŋ","ɴ","p","b","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ",,"ç","ʝ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","ʋ","ɹ","ɻ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɬ","ɮ","l","ɭ","ʎ","ʟ","ɺ"],
-		minusconsonantal: ["h","ɦ","ʔ","j","ɰ","i","y","ɨ","ʉ","ɯ","u","ɪ","ʏ","ʊ","e","ø","ɘ","ɵ","ɤ","o","ə","ɛ","œ","ɜ","ɞ","ʌ","ɔ","æ","ɐ","a","ɶ","ɑ","ɒ"],
-		plusapproximant: ["ʋ","ɹ","ɻ","j","ɰ","l","ɭ","ʎ","ʟ"],
-		minusapproximant: ["m","ɱ","n","ɳ","ɲ","ŋ","ɴ","p","b","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ʔ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɬ","ɮ","ɺ"],
-		plussonorant: ["m","ɱ","n","ɳ","ɲ","ŋ","ɴ","l","ɭ","ʎ","ʟ","ɺ","ʋ","ɹ","ɻ","j","ɰ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɬ","ɮ","i","y","ɨ","ʉ","ɯ","u","ɪ","ʏ","ʊ","e","ø","ɘ","ɵ","ɤ","o","ə","ɛ","œ","ɜ","ɞ","ʌ","ɔ","æ","ɐ","a","ɶ","ɑ","ɒ"],
-		minussonorant: ["p","b","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ʔ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ"],
-		plusvoiced: ["b","m","ɱ","n","ɳ","ɲ","ŋ","ɴ","d","ɖ","ɟ","g","ɢ","ʡ","β","v","ð","z","ʒ","ʐ","ʑ","ʝ","ɣ","ʁ","ʢ","ɦ","ʋ","ɹ","ɻ","j","ɰ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɮ","l","ɭ","ʎ","ʟ","ɺ","ʕ","i","y","ɨ","ʉ","ɯ","u","ɪ","ʏ","ʊ","e","ø","ɘ","ɵ","ɤ","o","ə","ɛ","œ","ɜ","ɞ","ʌ","ɔ","æ","ɐ","a","ɶ","ɑ","ɒ"],
-		minusvoiced: ["p","t","ʈ","s","ɬ","θ","ʃ","ʂ","ɕ","c","ç","f","ɸ","k","x","q","ħ","h","ʔ","χ","ʜ"],
-		plusspreadGlottis: ["ɬ","h","ɦ"],
-		minusspreadGlottis: ["m","ɱ","n","ɳ","ɲ","ŋ","ɴ","p","b","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ʔ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","ʋ","ɹ","ɻ","j","ɰ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɮ","l","ɭ","ʎ","ʟ","ɺ"],
-		plusconstrictedGlottis: [,"ʔ"],
-		minusconstrictedGlottis: ["m","ɱ","n","ɳ","ɲ","ŋ","ɴ","p","b","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ","ʋ","ɹ","ɻ","j","ɰ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɬ","ɮ","l","ɭ","ʎ","ʟ","ɺ"],
-		pluscontinuant: [,"ɣ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","χ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ","ʋ","ɹ","ɻ","j","ɰ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɬ","ɮ","l","ɭ","ʎ","ʟ","ɺ","i","y","ɨ","ʉ","ɯ","u","ɪ","ʏ","ʊ","e","ø","ɘ","ɵ","ɤ","o","ə","ɛ","œ","ɜ","ɞ","ʌ","ɔ","æ","ɐ","a","ɶ","ɑ","ɒ"],
-		minuscontinuant: ["ʡ","m","ɱ","n","ɳ","ɲ","ŋ","ɴ","t","d","ʈ","ɖ","c","ɟ","p","b","k","g","q","ɢ","ʔ"],
-		plusnasal: ["m","ɱ","n","ɳ","ɲ","ŋ","ɴ"],
-		minusnasal: ["p","b","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ʔ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ","ʋ","ɹ","ɻ","j","ɰ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɬ","ɮ","l","ɭ","ʎ","ʟ","ɺ"],
-		plusstrident: ["s","z","f","v","χ","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ"],
-		minusstrident: ["m","ɱ","n","ɳ","ɲ","ŋ","ɴ","p","b","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ʔ","ɸ","β","θ","ð","ç","ʝ","x","ɣ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ","ʋ","ɹ","ɻ","j","ɰ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɬ","ɮ","l","ɭ","ʎ","ʟ","ɺ"],
-		pluslateral: ["ɬ","ɮ","l","ɭ","ʎ","ʟ","ɺ"],
-		minuslateral: ["m","ɱ","n","ɳ","ɲ","ŋ","ɴ","p","b","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ʔ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ","ʋ","ɹ","ɻ","j","ɰ","ⱱ","ɾ","ɽ","ʙ","r","ʀ"],
-		pluslabial: ["m","ɱ","p","b","ɸ","β","f","v","ʋ","ⱱ","ʙ","j","ɰ","i","y","ɨ","ʉ","ɯ","u","ɪ","ʏ","ʊ","e","ø","ɘ","ɵ","ɤ","o","ə","ɛ","œ","ɜ","ɞ","ʌ","ɔ","æ","ɐ","a","ɶ","ɑ","ɒ"],
-		minuslabial: ["n","ɳ","ɲ","ŋ","ɴ","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ʔ","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ","ɹ","ɻ","ɾ","ɽ","r","ʀ","ɬ","ɮ","l","ɭ","ʎ","ʟ","ɺ"],
-		plusround: [,"u","ʊ","o","ɔ","y","ʏ","ø","ə","ʉ","ɵ","ɞ","ɶ","œ","ɒ"],
-		minusround: ["p","b","f","v","ɸ","β","m","j","ɰ","i","ɨ","ɪ","e","ɘ","ɤ","ɛ","ɜ","ʌ","æ","ɐ","a","ɑ","ɯ"],
-		pluscoronal: ["t","d","s","z","ɬ","ɮ","θ","ð","ʃ","ʒ","n","ɳ","l","r","ɹ","ɾ","ɺ","ʈ","ɖ","ʂ","ʐ","ɻ","ɽ","ɭ"],
-		minuscoronal: ["c","ɟ","ç","ʝ","m","ɱ","ɲ","ŋ","ɴ","p","b","k","g","q","ɢ","ʡ","ʔ","ɸ","β","f","v","ɕ","ʑ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ","ʋ","j","ɰ","ⱱ","ʙ","ʀ","ʎ","ʟ"],
-//		plusanterior: ["m","ɱ","n","p","b","t","d","ɸ","β","f","v","θ","ð","s","z","ʋ","ɹ","ⱱ","ɾ","ʙ","r","ɬ","ɮ","l","ɺ"],
-//		minusanterior: ["ɳ","ɲ","ŋ","ɴ","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ʔ","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","ħ","ʕ","ʜ","ʢ","h","ɦ","ɻ","j","ɰ","ɽ","ʀ","ɭ","ʎ","ʟ"],
-		plusanterior: ["p", "b", "t", "d", "ɸ", "β", "f", "v", "θ", "ð", "s", "z", "m", "n", "l", "ɬ", "r", "ɾ"],
-		minusanterior: ["c", "ɟ", "k", "g", "ʃ", "ʒ", "x", "ɣ", "ɲ", "ŋ", "j", "w", "ʡ", "h"],
-		plusdistributed: ["ʃ","ʒ","ç","ʝ","c","ɟ","ɕ","ʑ"],
-		minusdistributed: ["t","d","s","z","ɬ","ɮ","θ","ð","n","ɳ","l","r","ɹ"],
-		plusdorsal: ["ɲ","ŋ","ɴ","c","ɟ","k","g","q","ɢ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","j","ɰ","ʀ","ʎ","ʟ","i","y","ɨ","ʉ","ɯ","u","ɪ","ʏ","ʊ","e","ø","ɘ","ɵ","ɤ","o","ə","ɛ","œ","ɜ","ɞ","ʌ","ɔ","æ","ɐ","a","ɶ","ɑ","ɒ"],
-		minusdorsal: ["m","ɱ","n","ɳ","p","b","t","d","ʈ","ɖ","ʡ","ʔ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ħ","ʕ","ʜ","ʢ","h","ɦ","ʋ","ɹ","ɻ","ⱱ","ɾ","ɽ","ʙ","r","ɬ","ɮ","l","ɭ","ɺ"],
-		plushigh: ["c","ɟ","k","g","ʃ","ʒ","x","ɣ","ɲ","j","w","ŋ","ɰ","i","y","ɨ","ʉ","ɯ","u"],
-		minushigh: ["q","ɢ","χ","ʁ","ħ","ɴ","ɳ","ʀ","ʎ","ɪ","ʏ","ʊ","e","ø","ɘ","ɵ","ɤ","o","ə","ɛ","œ","ɜ","ɞ","ʌ","ɔ","æ","ɐ","a","ɶ","ɑ","ɒ"],
-		pluslow: ["a","ɶ","ɑ","ɒ"],
-		minuslow: ["c","ɟ","ç","ʝ","k","g","q","ɢ","x","ɣ","χ","ʁ","ɳ","ɲ","ŋ","ɴ","ʎ","ʀ","i","y","ɨ","ʉ","ɯ","u","ɪ","ʏ","ʊ","e","ø","ɘ","ɵ","ɤ","o","ə","ɛ","œ","ɜ","ɞ","ʌ","ɔ","æ","ɐ"],
-		plusback: ["k","g","q","ɢ","x","ɣ","χ","ʁ","ŋ","ɴ","ʀ","ɯ","u","ʊ","ɤ","o","ʌ","ɔ","ɑ","ɒ"],
-		minusback: ["c","ɟ","ç","ʝ","ɳ","ɲ","ʎ","i","y","ɨ","ʉ","ɪ","ʏ","e","ø","ɘ","ɵ","ə","ɛ","œ","ɜ","ɞ","æ","ɐ","a","ɶ"],
-		plustense: ["i","u","e","o","æ","y","ø","ɯ","ʌ"],
-		minustense: ["c","ɟ","ç","ʝ","k","g","q","ɢ","x","ɣ","χ","ʁ","ɳ","ɲ","ŋ","ɴ","ʎ","ʀ","ɨ","ʉ","ɪ","ʏ","ʊ","ɘ","ɵ","ɤ","ə","ɛ","œ","ɜ","ɞ","ɔ","ɐ","a","ɶ","ɑ","ɒ"],
-		pluspharyngeal: ["ħ","ʕ","i","y","ɨ","ʉ","ɯ","u","ɪ","ʏ","ʊ","e","ø","ɘ","ɵ","ɤ","o","ə","ɛ","œ","ɜ","ɞ","ʌ","ɔ","æ","ɐ","a","ɶ","ɑ","ɒ"],
-		minuspharyngeal: ["m","ɱ","n","ɳ","ɲ","ŋ","ɴ","p","b","t","d","ʈ","ɖ","c","ɟ","k","g","q","ɢ","ʡ","ʔ","ɸ","β","f","v","θ","ð","s","z","ʃ","ʒ","ʂ","ʐ","ɕ","ʑ","ç","ʝ","x","ɣ","χ","ʁ","ʜ","ʢ","h","ɦ","ʋ","ɹ","ɻ","j","ɰ","ⱱ","ɾ","ɽ","ʙ","r","ʀ","ɬ","ɮ","l","ɭ","ʎ","ʟ","ɺ"],
-		plusaTR: ["i","u","e","o","y","ø","ɯ","ʌ"],
-		minusaTR: ["ħ","ʕ","ɨ","ʉ","ɪ","ʏ","ʊ","ɘ","ɵ","ɤ","ə","ɛ","œ","ɜ","ɞ","ɔ","ɐ","a","ɶ","ɑ","ɒ","æ"],
-		plusglottal: ["h","ɦ","ʔ"],
-		minusglottal: [],
-		
-		
-		
-		}
-		
-		var toAdd = [];
-		var toFilter = [];
-		
-		
-		
-		function addFeature(featureId) {													//TO REMOVE...............................................
-			
 
-				if (document.getElementById("sign" + featureId).firstChild.data === "+") {
-					 toAdd.push(featuresObject["plus" +featureId]);
-				}
-				else {
-					toAdd.push(featuresObject["minus" +featureId]);
-				}
-			
-			
-//			console.log(toAdd);	
-			activeFeatures++;
-		}
-		
-		function switchSign(buttonId) {
-			var currentText = document.getElementById(buttonId);
-//			console.log(currentText);
-//			console.log("selectedS" + buttonId.slice(1));
+  if (document.getElementById("sign" + featureId).firstChild.data === "+") {
+    toAdd.push(featuresObject["plus" +featureId]);
+  }
+  else {
+    toAdd.push(featuresObject["minus" +featureId]);
+  }
 
-			
-			if (buttonId.indexOf("selected") === -1) {
-				if (currentText.firstChild.data === "+") {
-					currentText.firstChild.data = "-";
-					document.getElementById("selectedS" + buttonId.slice(1)).firstChild.data = "-";
-				}
-				else {
-					currentText.firstChild.data = "+";
-					document.getElementById("selectedS" + buttonId.slice(1)).firstChild.data = "+";
-				}
-			}
-			else {
-				if (currentText.firstChild.data === "+") {
-					currentText.firstChild.data = "-";
-					document.getElementById("s" + buttonId.slice(9)).firstChild.data = "-";
-				}
-				else {
-					currentText.firstChild.data = "+";
-					document.getElementById("s" + buttonId.slice(9)).firstChild.data = "+";
-				}
-			}
-		}
-		
-		function moveButton(buttonId) {
-//			document.getElementById(buttonId).style.display = 'none';
-//			document.getElementById("sign" + buttonId).style.display = 'none';
-//			document.getElementById("selected" + buttonId).style.display = '';
-//			document.getElementById("selectedSign" + buttonId).style.display = 'none'; 
 
-			
-			if (buttonId.indexOf("selected") === -1) {
-				document.getElementById(buttonId).style.display = 'none';
-				document.getElementById("sign" + buttonId).style.display = 'none';
-				document.getElementById("selected" + buttonId).style.display = '';
-				document.getElementById("selectedSign" + buttonId).style.display = '';
-			}
-			
-			else {
-//				document.getElementById(buttonId).style.display = '';
-//				document.getElementById("sign" + buttonId).style.display = '';
-//				document.getElementById("selected" + buttonId).style.display = 'none';
-//				document.getElementById("selectedSign" + buttonId).style.display = 'none';
+  //			console.log(toAdd);	
+  activeFeatures++;
+}
 
-				document.getElementById(buttonId).style.display = 'none';
-				document.getElementById(buttonId.replace("selected", "selectedSign")).style.display = 'none';
-				document.getElementById(buttonId.slice(8)).style.display = '';
-				document.getElementById("sign" + buttonId.slice(8)).style.display = '';
-			}
-		}
+function switchSign(buttonId) {
+  var currentText = document.getElementById(buttonId);
+  //			console.log(currentText);
+  //			console.log("selectedS" + buttonId.slice(1));
+
+
+  if (buttonId.indexOf("selected") === -1) {
+    if (currentText.firstChild.data === "+") {
+      currentText.firstChild.data = "-";
+      document.getElementById("selectedS" + buttonId.slice(1)).firstChild.data = "-";
+    }
+    else {
+      currentText.firstChild.data = "+";
+      document.getElementById("selectedS" + buttonId.slice(1)).firstChild.data = "+";
+    }
+  }
+  else {
+    if (currentText.firstChild.data === "+") {
+      currentText.firstChild.data = "-";
+      document.getElementById("s" + buttonId.slice(9)).firstChild.data = "-";
+    }
+    else {
+      currentText.firstChild.data = "+";
+      document.getElementById("s" + buttonId.slice(9)).firstChild.data = "+";
+    }
+  }
+}
+
+function moveButton(buttonId) {
+  //			document.getElementById(buttonId).style.display = 'none';
+  //			document.getElementById("sign" + buttonId).style.display = 'none';
+  //			document.getElementById("selected" + buttonId).style.display = '';
+  //			document.getElementById("selectedSign" + buttonId).style.display = 'none'; 
+
+
+  if (buttonId.indexOf("selected") === -1) {
+    document.getElementById(buttonId).style.display = 'none';
+    document.getElementById("sign" + buttonId).style.display = 'none';
+    document.getElementById("selected" + buttonId).style.display = '';
+    document.getElementById("selectedSign" + buttonId).style.display = '';
+  }
+
+  else {
+    //				document.getElementById(buttonId).style.display = '';
+    //				document.getElementById("sign" + buttonId).style.display = '';
+    //				document.getElementById("selected" + buttonId).style.display = 'none';
+    //				document.getElementById("selectedSign" + buttonId).style.display = 'none';
+
+    document.getElementById(buttonId).style.display = 'none';
+    document.getElementById(buttonId.replace("selected", "selectedSign")).style.display = 'none';
+    document.getElementById(buttonId.slice(8)).style.display = '';
+    document.getElementById("sign" + buttonId.slice(8)).style.display = '';
+  }
+}
 
 /**
  * Determines which features are currently active
@@ -167,7 +99,7 @@ function parseActiveFeatures() {
 
   return activeFeatures;
 }
-		
+
 /**
  * Updates the table based on the selected features
  */
